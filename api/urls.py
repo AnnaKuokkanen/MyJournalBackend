2bee26649870b1de0dcdf42e8b86797c7362a1ea
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2023-06-27 03:31:01 +0300

    Change API base path

--- api/urls.py
+++ api/urls.py
@@ -3,11 +3,11 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'journal_entries', views.JournalEntriesListViewSet)
+router.register("journal_entries", views.JournalEntriesViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- api/views.py
+++ api/views.py
@@ -3,6 +3,6 @@ from .serializers import JournalEntrySerializer
 from .models import JournalEntry
 
 
-class JournalEntriesListViewSet(viewsets.ModelViewSet):
+class JournalEntriesViewSet(viewsets.ModelViewSet):
     queryset = JournalEntry.objects.all().order_by('date')
     serializer_class = JournalEntrySerializer
